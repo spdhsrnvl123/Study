18bcc40c8b43a82754426fb09dc4548350c2eb5f
Leutbing <spdhsrnvl123@naver.com>
2022-04-28 23:41:12 +0900

    first

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
+
+const Remove = style.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    color:#dee2e6;
+    font-size:24px;
+    cursor:pointer;
+    &:hover{
+        color:#ff6b6b;
+    }
+`;
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+
+const CheckCircle = styled.div`
+    width: 32px;
+    height: 32px;
+    border-radius : 16px;
+    border : 1px solid #ced4da;
+    font-size:24px;
+    display:flex;
+    align
+`;
